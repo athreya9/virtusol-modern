0e697aaaa4dbfc54e3675dd5dfd6323de6bfac73
Athreya <athreya9@gmail.com>
2026-01-19 09:10:19 +0530

    Debug WebGL crash: temporarily disable CinematicLens

--- src/components/features/cinematic/CinematicLens.tsx
+++ src/components/features/cinematic/CinematicLens.tsx
@@ -12,41 +12,5 @@ import * as THREE from "three";
  * Adds grain, edge distortion, and dynamic chromatic aberration.
  */
 export function CinematicLens() {
-    const abRef = useRef<any>(null);
-    const [offset] = useState(() => new Vector2(0, 0));
-
-    useFrame((state) => {
-        // Dynamic aberration based on time and a bit of 'jitter'
-        const time = state.clock.getElapsedTime();
-        const intensity = 0.0015 + Math.sin(time * 2) * 0.0005;
-        if (abRef.current) {
-            abRef.current.offset.set(intensity, intensity);
-        }
-    });
-
-    return (
-        <EffectComposer>
-            <Noise
-                opacity={0.12}
-                premultiply
-                blendFunction={BlendFunction.SCREEN}
-            />
-            <ChromaticAberration
-                ref={abRef}
-                offset={offset}
-                radialModulation={true}
-                modulationOffset={0.5}
-            />
-            <Bloom
-                intensity={0.6}
-                luminanceThreshold={0.85}
-                luminanceSmoothing={0.1}
-            />
-            <Vignette
-                eskil={false}
-                offset={0.4}
-                darkness={0.8}
-            />
-        </EffectComposer>
-    );
+    return null;
 }
